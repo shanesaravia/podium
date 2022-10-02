826fe78e5794abf3d841eb4c0d4f79a1465e3b0a
Shane Saravia <shanesaravia@live.ca>
2022-10-01 22:05:53 -0400

    initial commit

---
+++ client/src/App.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import Sidebar from 'src/components/Sidebar.svelte'
+	export let name: string;
+	const test = "Shane"
+</script>
+
+<main>
+	<Sidebar />
+	<h1>Hello {name} {test}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<img src="/images/podiumLogo.png" alt="podium-logo" />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ client/src/utils/uiConstants.ts
@@ -0,0 +1,5 @@
+export const colors = {
+  red: '#fe4654',
+  blue: '#0160b7',
+  yellow: '#f2b404'
+}
