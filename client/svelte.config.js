52c57c047f432c882bb9061b704a019615ae0831
Shane Saravia <shanesaravia@live.ca>
2022-10-04 02:31:06 -0400

    fix warnings

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,10 +1,17 @@
 const sveltePreprocess = require("svelte-preprocess");
+const production = !process.env.ROLLUP_WATCH;
 
 module.exports = {
   preprocess: sveltePreprocess({
-    // sourceMap: !production,
+    sourceMap: !production,
     scss: {
       prependData: `@import 'client/scss/theme.scss';`,
     },
   }),
+  onwarn: (warning, handler) => {
+    const { code, frame } = warning;
+    if (code === "css-unused-selector") return;
+
+    handler(warning);
+  },
 };
